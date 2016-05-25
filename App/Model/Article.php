acb0dd9d5d9e7d2061184213b9a78cc49e9d4b33
DC <heytsh@gmail.com>
2016-05-25 19:28:14 +0700

    update the article model and views bug

--- App/Model/Article.php
+++ App/Model/Article.php
@@ -25,13 +25,19 @@ class Article extends Model
         Schema::create('article', function($table) {
             /** @var \Illuminate\Database\Schema\Blueprint $table */
             $table->increments('id')->unsigned();
-            $table->integer('uid')->unsigned()->default(0)->comment('作者id');
+            $table->integer('pid')->unsigned()->default(0)->comment('父文pid');
+            $table->integer('uid')->unsigned()->default(0)->comment('用户id');
+            $table->integer('gid')->unsigned()->default(0)->comment('组id');
+            $table->integer('tag')->unsigned()->default(0)->comment('标签');
             $table->string('from',48)->nullable()->comment('来源');
             $table->string('link')->nullable()->comment('链接');
+            $table->string('price')->unsigned()->default(0)->comment('价格');
             $table->string('title',96)->nullable()->comment('标题');
             $table->string('cover',96)->nullable()->comment('封面');
             $table->string('author',64)->nullable()->comment('作者');
+            $table->string('summary')->nullable()->comment('摘要');
             $table->text('content')->nullable()->comment('内容');
+            $table->string('password')->nullable()->comment('阅读权限,非空表示须要密码');
             $table->integer('times')->nullable()->comment('点击次数');
             $table->timestamp('overdue_at')->nullable()->comment('过期时间');
             $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'))->comment('创建时间');
