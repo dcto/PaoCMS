76f7cad599e7e7571d66edb23f414ca63ef6300b
DC <heytsh@gmail.com>
2016-05-31 22:48:34 +0700

    add the upload config file

---
+++ Config/upload.php
@@ -0,0 +1,13 @@
+<?php
+defined('PAO') || die('The PaoCMS Load Error');
+
+
+return array(
+
+    'save'=>'./', //保存文件目录
+
+    'size'=>1024, //上传大小限制
+
+    'kind'=>['gif','png','jpg','jpeg','swf'], //允许上传文件
+
+);
