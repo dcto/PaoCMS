a682eda34a6e7caf10499edc876d2b94915b36fb
DC <heytsh@gmail.com>
2016-07-01 16:24:59 +0700

    update response

--- Frameworks/Http/Response.php
+++ Frameworks/Http/Response.php
@@ -2,13 +2,85 @@
 
 namespace PAO\Http;
 
+
 use Illuminate\Container\Container;
 use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Support\Traits\Macroable;
 use Symfony\Component\HttpFoundation;
 
-
-class Response //implements  \Illuminate\Contracts\Routing\ResponseFactory
+class Response implements  ResponseFactory
 {
+    use Macroable;
+
+    protected $message =  [
+        //Informational 1xx
+        100 => 'Continue',
+        101 => 'Switching Protocols',
+        102 => 'Processing',
+        //Successful 2xx
+        200 => 'OK',
+        201 => 'Created',
+        202 => 'Accepted',
+        203 => 'Non-Authoritative Information',
+        204 => 'No Content',
+        205 => 'Reset Content',
+        206 => 'Partial Content',
+        207 => 'Multi-Status',
+        208 => 'Already Reported',
+        226 => 'IM Used',
+        //Redirection 3xx
+        300 => 'Multiple Choices',
+        301 => 'Moved Permanently',
+        302 => 'Found',
+        303 => 'See Other',
+        304 => 'Not Modified',
+        305 => 'Use Proxy',
+        306 => '(Unused)',
+        307 => 'Temporary Redirect',
+        308 => 'Permanent Redirect',
+        //Client Error 4xx
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        402 => 'Payment Required',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        406 => 'Not Acceptable',
+        407 => 'Proxy Authentication Required',
+        408 => 'Request Timeout',
+        409 => 'Conflict',
+        410 => 'Gone',
+        411 => 'Length Required',
+        412 => 'Precondition Failed',
+        413 => 'Request Entity Too Large',
+        414 => 'Request-URI Too Long',
+        415 => 'Unsupported Media Type',
+        416 => 'Requested Range Not Satisfiable',
+        417 => 'Expectation Failed',
+        418 => 'I\'m a teapot',
+        422 => 'Unprocessable Entity',
+        423 => 'Locked',
+        424 => 'Failed Dependency',
+        426 => 'Upgrade Required',
+        428 => 'Precondition Required',
+        429 => 'Too Many Requests',
+        431 => 'Request Header Fields Too Large',
+        451 => 'Unavailable For Legal Reasons',
+        //Server Error 5xx
+        500 => 'Internal Server Error',
+        501 => 'Not Implemented',
+        502 => 'Bad Gateway',
+        503 => 'Service Unavailable',
+        504 => 'Gateway Timeout',
+        505 => 'HTTP Version Not Supported',
+        506 => 'Variant Also Negotiates',
+        507 => 'Insufficient Storage',
+        508 => 'Loop Detected',
+        510 => 'Not Extended',
+        511 => 'Network Authentication Required',
+    ];
+
 
     /**
      * 全局响应
@@ -17,10 +89,25 @@ class Response //implements  \Illuminate\Contracts\Routing\ResponseFactory
      */
     private $response;
 
+
+    /**
+     * [error Response]
+     * @param $status [错误状态]
+     * @param null $message [错误信息]
+     * @return $this
+     */
+    public function error($status, $message = null)
+    {
+        $message = $message?:$this->message[$status];
+        $this->response = new HttpFoundation\Response($message, $status);
+        return $this;
+    }
+
+
     /**
      * [make Response]
      *
-     * @param string $content [响应内容]
+     * @param string $content [响应x内容]
      * @param int    $status [状态值]
      * @param array  $headers [header]
      * @author 11.
@@ -54,7 +141,7 @@ class Response //implements  \Illuminate\Contracts\Routing\ResponseFactory
      * @param array $headers [header]
      * @author 11.
      */
-    public function view($view, array $data = [], $status = 200, array $headers = [])
+    public function view($view, $data = [], $status = 200, array $headers = [])
     {
        return $this->make(Container::getInstance()->make('view')->render($view, $data), $status, $headers);
     }
@@ -69,7 +156,7 @@ class Response //implements  \Illuminate\Contracts\Routing\ResponseFactory
      * @return HttpFoundation\JsonResponse
      * @author 11.
      */
-    public function json($data = [], $status = 200, array $headers = [])
+    public function json($data = [], $status = 200, array $headers = [], $options = 0)
     {
         if ($data instanceof Arrayable && ! $data instanceof \JsonSerializable) {
             $data = $data->toArray();
@@ -90,7 +177,7 @@ class Response //implements  \Illuminate\Contracts\Routing\ResponseFactory
      * @return HttpFoundation\JsonResponse
      * @author 11.
      */
-    public function jsonp($callback, $data = [], $status = 200, array $headers = [])
+    public function jsonp($callback, $data = [], $status = 200, array $headers = [], $options = 0)
     {
         return $this->json($data, $status, $headers)->setCallback($callback);
     }
@@ -148,6 +235,16 @@ class Response //implements  \Illuminate\Contracts\Routing\ResponseFactory
 
         return $this;
     }
+
+    public function redirectTo($path, $status = 302, $headers = [], $secure = null){}
+
+    public function redirectToRoute($route, $parameters = [], $status = 302, $headers = []){}
+
+    public function redirectToAction($action, $parameters = [], $status = 302, $headers = []){}
+
+    public function redirectGuest($path, $status = 302, $headers = [], $secure = null){}
+
+    public function redirectToIntended($default = '/', $status = 302, $headers = [], $secure = null){}
 
 
     /**
