f305ffcd9de96a26bcff91c51d7272005101a38c
DC <heytsh@gmail.com>
2016-07-26 14:14:35 +0700

    update route

--- App/Admin/Route.php
+++ App/Admin/Route.php
@@ -11,6 +11,12 @@ Router::group(['prefix'=>'/', 'namespace'=>'Admin\Controller'], function(){
         Router::get('/delete','User@delete')->tag('user_delete')->name('删除会员');
     });
 
+    Router::group(['tag'=>'group', 'name'=>'组', 'icon'=>'group', 'prefix'=>'/group'], function(){
+        Router::get('/','Group@index')->tag('group')->name('组列表');
+        Router::any('/create','Group@create')->tag('group_create')->name('创建组');
+        Router::any('/update','Group@update')->tag('group_update')->name('修改组');
+        Router::get('/delete','Group@delete')->tag('group_delete')->name('删除组');
+    });
 
 
 });
