8543ede0868697376707fbee2b6230862baaad1f
DC <heytsh@gmail.com>
2016-07-26 13:10:43 +0700

    update core functions and router path

--- Frameworks/Function.php
+++ Frameworks/Function.php
@@ -1,14 +1,13 @@
 <?php
 
 use Illuminate\Container\Container;
-use Illuminate\Support\Facades\Facade;
-    /**
-     * Get the available container instance.
-     *
-     * @param  string  $make
-     * @param  array   $parameters
-     * @return object
-     */
+/**
+ * Get the available container instance.
+ *
+ * @param  string  $make
+ * @param  array   $parameters
+ * @return object
+ */
 function app($make = null, $parameters = [])
 {
     if (is_null($make)) {
@@ -25,7 +24,7 @@ function app($make = null, $parameters = [])
  */
 function lang()
 {
-   return call_user_func_array(array(app('lang'), 'get'), func_get_args());
+    return call_user_func_array(array(app('lang'), 'get'), func_get_args());
 }
 
 /**
@@ -35,4 +34,31 @@ function lang()
 function config()
 {
     return call_user_func_array(array(app('config'), 'get'), func_get_args());
+}
+
+
+/**
+ * response
+ * @param $content
+ * @param int $code
+ * @param array $header
+ * @return mixed
+ */
+function response($content, $status = 200, $header = array())
+{
+    return app('response')->make($content, $status, $header);
+}
+
+
+/**
+ * view
+ * @param $view
+ * @param array $data
+ * @param int $status
+ * @param array $headers
+ * @return mixed
+ */
+function view($view, array $data = [], $status = 200, array $headers = [])
+{
+    return app('response')->view($view, $data, $status, $headers);
 }
