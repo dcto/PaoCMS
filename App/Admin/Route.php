439302f62d5826911554c4c61b8dcd168581543f
DC <heytsh@gmail.com>
2016-07-26 14:03:44 +0700

    update admin route

--- App/Admin/Route.php
+++ App/Admin/Route.php
@@ -1,14 +1,14 @@
 <?php
-Router::group(['prefix'=>'/','namespace'=>'Admin\Controller'], function(){
+Router::group(['prefix'=>'/', 'namespace'=>'Admin\Controller'], function(){
 
     Router::get('/', 'Index@index');
 
 
-    Router::group(['prefix'=>'/user', 'name'=>'会员'], function(){
-        Router::get('/','User@index')->tag('user.create')->name('会员列表');
-        Router::get('/create','User@create')->tag('user.create')->name('创建会员');
-        Router::get('/update','User@update')->tag('user.update')->name('修改会员');
-        Router::get('/delete','User@update')->tag('user.delete')->name('删除会员');
+    Router::group(['tag'=>'user', 'name'=>'会员', 'icon'=>'user', 'prefix'=>'/user'], function(){
+        Router::get('/','User@index')->tag('user')->name('会员列表');
+        Router::any('/create','User@create')->tag('user_create')->name('创建会员');
+        Router::any('/update','User@update')->tag('user_update')->name('修改会员');
+        Router::get('/delete','User@delete')->tag('user_delete')->name('删除会员');
     });
 
 
