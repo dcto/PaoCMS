c9f0fff7e21ae2925db04d698e1bd7c42205b9c4
DC <heytsh@gmail.com>
2016-07-28 11:05:01 +0700

    update route

--- App/Admin/Controller/User.php
+++ App/Admin/Controller/User.php
@@ -2,14 +2,8 @@
 
 namespace Admin\Controller;
 
-use DB;
-use Config;
-use Input;
-use Lang;
-use Request;
-use Response;
-use Crypt;
 use PAO\Exception\NotFoundHttpException;
+use PAO\Http\Request;
 
 class User extends Controller
 {
@@ -18,43 +12,43 @@ class User extends Controller
      * [index]
      * @return mixed
      */
-    public function index()
+    public function index(Request $request)
 	{
 
         $this->assign('title', lang('menu.user'));
-        if(Input::has('w')){
-            $w = trim(Input::get('w'));
+        if($request->has('w')){
+            $w = trim($request->get('w'));
             $assign['list'] = \App\Model\User::where('username', 'LIKE', "%$w%")
                 ->Orwhere('email', 'LIKE', "%$w%")
-                ->with('group')->paginate(Config::get('config.page'))->appends(Input::all('!page'));
+                ->with('group')->paginate(config('config.page'))->appends($request->all('!page'));
         }else {
-            $assign['list'] = \App\Model\User::with('group')->paginate(Config::get('config.page'));
+            $assign['list'] = \App\Model\User::with('group')->paginate(config('config.page'));
         }
             $assign['group'] = \App\Model\Group::all();
-	 	return Response::view('user', $assign);
+	 	return view('user', $assign);
 	}
 
     /**
      * [detail详情]
      * @return mixed
      */
-    public function detail()
+    public function detail(Request $request)
     {
-        $id = Request::get('id');
-        return Response::view('user');
+        $id = $request->get('id');
+        return view('user');
     }
 
     /**
      * [create创建]
      * @return mixed
      */
-	public function create()
+	public function create(Request $request)
 	{
-        $admin = Input::all();
+        $admin = $request->all();
         if(!$this->checkForm($admin)) return $this->alert();
-            $user = \App\Model\User::whereUsername(Input::get('username'))->first();
+            $user = \App\Model\User::whereUsername($request->get('username'))->first();
             if(!$user){
-                $admin['password'] = md5(Crypt::encrypt(Input::get('password1')));
+                $admin['password'] = md5(\Crypt::encrypt($request->get('password1')));
 
                 $adminId = \App\Model\User::create($admin);
                 if($adminId){
@@ -64,7 +58,7 @@ class User extends Controller
                    $this->message = lang('alert.create_failure', lang('admin.title'));
                 }
             }else{
-                $this->message = lang('alert.exist', Input::get('username'));
+                $this->message = lang('alert.exist', $request->get('username'));
             }
 
         return $this->alert();
@@ -74,29 +68,29 @@ class User extends Controller
      * [update更新]
      * @return mixed
      */
-	public function update()
+	public function update(Request $request)
 	{
 
-        if(Request::isMethod('POST'))
+        if($request->isMethod('POST'))
         {
 
-            $ids =  Input::get('id');
+            $ids =  $request->get('id');
             if(!$ids) return $this->alert(false, lang('alert.id_empty'));
 
             /**
              *批量更新
              */
             if(is_array($ids)){
-                \App\Model\User::whereIn('id', $ids)->update( array('status'=>Input::get('status')));
+                \App\Model\User::whereIn('id', $ids)->update( array('status'=>$request->get('status')));
                 return $this->alert(true, lang('alert.update_success', implode(',', $ids)));
             }
-            $data = Input::not(['password1','password2']);
-            if(Input::get('password1') && Input::get('password2')){
-                if(!$this->checkForm(Input::all())) return $this->alert();
+            $data = $request->not(['password1','password2']);
+            if($request->get('password1') && $request->get('password2')){
+                if(!$this->checkForm($request->all())) return $this->alert();
 
-                $data['password'] = md5(Input::get('password2'));
+                $data['password'] = md5($request->get('password2'));
             }else{
-                if(!$this->checkForm(Input::all(),['password'])) return $this->alert();
+                if(!$this->checkForm($request->all(),['password'])) return $this->alert();
             }
             $user =\App\Model\User::find($ids);
             if($user->update($data)){
@@ -110,7 +104,7 @@ class User extends Controller
             return $this->alert($this->status, $this->message);
         }
 
-        $id = Request::get('id');
+        $id = $request->get('id');
         if(!$id) throw new NotFoundHttpException(lang('alert.404'));
         $user = \App\Model\User::with('group')->find($id)->toArray();
 
@@ -121,9 +115,9 @@ class User extends Controller
      * [delete删除]
      * @return mixed
      */
-	public function delete()
+	public function delete(Request $request)
 	{
-        $ids = (array) Input::get('id');
+        $ids = (array) $request->get('id');
         $delete = \App\Model\User::whereIn('id', $ids)->delete();
         if($delete){
             $this->status = true;
--- App/Admin/Route.php
+++ App/Admin/Route.php
@@ -6,30 +6,30 @@ Router::group(['prefix'=>'/', 'namespace'=>'Admin\Controller'], function(){
     Router::get('/access/logout', ['tag'=>'logout', 'call'=>'Index@index']);
 
     Router::group(['tag'=>'article', 'name'=>'menu.article', 'icon'=>'file', 'prefix'=>'/article'], function(){
-        Router::get('/','Group@index')->tag('article')->name('menu.article')->menu('true');;
-        Router::any('/create','Article@create')->tag('article_create')->name('menu.article_create')->menu(true);
-        Router::any('/update','Article@update')->tag('article_update')->name('menu.article_update');
-        Router::get('/delete','Article@delete')->tag('article_delete')->name('menu.article_delete');
+        Router::get('/','Article@index')->tag('article')->name('menu.article')->menu('true');;
+        Router::post('/create','Article@create')->tag('article_create')->name('menu.article_create')->menu(true);
+        Router::post('/update','Article@update')->tag('article_update')->name('menu.article_update');
+        Router::post('/delete','Article@delete')->tag('article_delete')->name('menu.article_delete');
     });
 
     Router::group(['tag'=>'trees', 'name'=>'menu.trees', 'icon'=>'th-list', 'prefix'=>'/trees'], function(){
         Router::get('/','Group@index')->tag('trees')->name('文章表');
-        Router::any('/create','Trees@create')->tag('trees_create')->name('menu.trees_create')->menu(true);
-        Router::any('/update','Trees@update')->tag('trees_update')->name('menu.trees_update');
-        Router::get('/delete','Trees@delete')->tag('trees_delete')->name('menu.trees_delete');
+        Router::post('/create','Trees@create')->tag('trees_create')->name('menu.trees_create')->menu(true);
+        Router::post('/update','Trees@update')->tag('trees_update')->name('menu.trees_update');
+        Router::post('/delete','Trees@delete')->tag('trees_delete')->name('menu.trees_delete');
     });
 
     Router::group(['tag'=>'user', 'name'=>'menu.user', 'icon'=>'user', 'prefix'=>'/user'], function(){
         Router::get('/','User@index')->tag('user')->name('menu.user')->menu('true');
-        Router::any('/create','User@create')->tag('user_create')->name('menu.user_create')->menu(true);
-        Router::any('/update','User@update')->tag('user_update')->name('menu.user_update');
-        Router::get('/delete','User@delete')->tag('user_delete')->name('menu.user_delete');
+        Router::post('/create','User@create')->tag('user_create')->name('menu.user_create')->menu(true);
+        Router::post('/update','User@update')->tag('user_update')->name('menu.user_update');
+        Router::post('/delete','User@delete')->tag('user_delete')->name('menu.user_delete');
     });
 
     Router::group(['tag'=>'group', 'name'=>'menu.group', 'icon'=>'group', 'prefix'=>'/group'], function(){
         Router::get('/','Group@index')->tag('group')->name('menu.group')->menu('true');;
-        Router::any('/create','Group@create')->tag('group_create')->name('menu.group_create')->menu(true);
-        Router::any('/update','Group@update')->tag('group_update')->name('menu.group_update');
-        Router::get('/delete','Group@delete')->tag('group_delete')->name('menu.group_delete');
+        Router::post('/create','Group@create')->tag('group_create')->name('menu.group_create')->menu(true);
+        Router::post('/update','Group@update')->tag('group_update')->name('menu.group_update');
+        Router::post('/delete','Group@delete')->tag('group_delete')->name('menu.group_delete');
     });
 });
