653310bd6ee278395667595246fca7e1c4c314d1
DC <heytsh@gmail.com>
2016-07-29 10:28:41 +0700

    update user group

--- App/Model/Group.php
+++ App/Model/Group.php
@@ -2,6 +2,7 @@
 
 namespace App\Model;
 
+use PAO\Support\Facades\DB;
 use PAO\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 
@@ -24,6 +25,7 @@ class Group extends Model
     {
         Schema::create('group', function(Blueprint $table) {
             $table->increments('id');
+            $table->integer('pid')->unsigned()->default(0)->comment('继承组id');
             $table->string('tag',32)->comment('组标签');
             $table->string('name',48)->comment('组名称');
             $table->string('nickname',96)->nullable()->comment('组头衔/别名');
@@ -36,6 +38,15 @@ class Group extends Model
             $table->index('status');
             $table->engine = 'InnoDB';
         });
+
+        $groups = array(
+            ['id'=>'1', 'tag'=>'admin', 'name'=>'系统管理员', 'nickname'=>'超级管理员', 'status'=>1,'created_at'=>date('Y-m-d H:i:s')],
+            ['id'=>'2', 'tag'=>'admin', 'name'=>'管理员', 'nickname'=>'管理员', 'status'=>1,'created_at'=>date('Y-m-d H:i:s')],
+            ['id'=>'3', 'tag'=>'finance', 'name'=>'财务部', 'nickname'=>'财务部', 'status'=>1,'created_at'=>date('Y-m-d H:i:s')],
+            ['id'=>'4', 'tag'=>'editor', 'name'=>'编辑部', 'nickname'=>'编辑部', 'status'=>1,'created_at'=>date('Y-m-d H:i:s')],
+            ['id'=>'5', 'tag'=>'market', 'name'=>'市场部', 'nickname'=>'市场部', 'status'=>1,'created_at'=>date('Y-m-d H:i:s')],
+        );
+        DB::table('group')->insert($groups);
     }
 
 
