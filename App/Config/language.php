6e28cd8b6adc9a47adc1ce0055ffa2484dc4ca9d
DC <heytsh@gmail.com>
2016-07-31 15:27:04 +0700

    add events

---
+++ App/Config/language.php
@@ -0,0 +1,14 @@
+<?php
+$language['cz'] = array('tag' => 'Czech',     'name' => 'čeština',    'locale' => 'cz_CZ', 'dir' => 'ltr');
+$language['da'] = array('tag' => 'Danish',    'name' => 'Dansk',      'locale' => 'da_DK', 'dir' => 'ltr');
+$language['de'] = array('tag' => 'German',    'name' => 'Deutsch',    'locale' => 'de_DE', 'dir' => 'ltr');
+$language['en'] = array('tag' => 'English',   'name' => 'English',    'locale' => 'en_US', 'dir' => 'ltr');
+$language['es'] = array('tag' => 'Spanish',   'name' => 'Español',    'locale' => 'es_ES', 'dir' => 'ltr');
+$language['fa'] = array('tag' => 'Persian',   'name' => 'پارسی',      'locale' => 'fa_IR', 'dir' => 'rtl');
+$language['fr'] = array('tag' => 'French',    'name' => 'Français',   'locale' => 'fr_FR', 'dir' => 'ltr');
+$language['it'] = array('tag' => 'Italian',   'name' => 'italiano',   'locale' => 'it_IT', 'dir' => 'ltr');
+$language['ja'] = array('tag' => 'Japanesse', 'name' => '日本語',      'locale' => 'ja_JA', 'dir' => 'ltr');
+$language['nl'] = array('tag' => 'Dutch',     'name' => 'Nederlands', 'locale' => 'nl_NL', 'dir' => 'ltr');
+$language['pl'] = array('tag' => 'Polish',    'name' => 'polski',     'locale' => 'pl_PL', 'dir' => 'ltr');
+$language['ro'] = array('tag' => 'Romanian',  'name' => 'Română',     'locale' => 'ro_RO', 'dir' => 'ltr');
+$language['ru'] = array('tag' => 'Russian',   'name' => 'ру́сский',    'locale' => 'ru_RU', 'dir' => 'ltr');
