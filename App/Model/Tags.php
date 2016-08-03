aac67ce1afc1895ce598b927dfd4a1a67fc14342
DC <heytsh@gmail.com>
2016-08-03 16:36:05 +0700

    add tags model

---
+++ App/Model/Tags.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Schema\Blueprint;
+use PAO\Support\Facades\Schema;
+
+class Tags extends Model
+{
+
+    protected $table = 'tags';
+
+
+    static public function up()
+    {
+
+        Schema::create('tags', function(Blueprint $table){
+            $table->increments('id');
+            $table->string('name',32)->comment('TAG名称');
+            $table->integer('hits')->unsigned()->default(0)->comment('点击次数');
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->unique('name');
+
+            $table->engine = 'innodb';
+        });
+    }
+
+    static public function down()
+    {
+
+       Schema::dropIfExists('tags');
+    }
+
+}
