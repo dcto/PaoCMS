0158bdc4d8c16e3b0fd4ac1e0322ff1b359642a4
DC <heytsh@gmail.com>
2016-09-30 14:48:17 +0700

    update facades of Router annotation

--- Frameworks/Support/Facades/Router.php
+++ Frameworks/Support/Facades/Router.php
@@ -4,15 +4,15 @@ use Illuminate\Support\Facades\Facade;
 
 /**
  * Class Router
- * @method static \PAO\Routing\Router group(array $attributes, Closure $callback)
- * @method static \PAO\Routing\Router get(string $url, array $params)
- * @method static \PAO\Routing\Router post(string $url, array $params)
- * @method static \PAO\Routing\Router put(string $url, array $params)
- * @method static \PAO\Routing\Router patch(string $url, array $params)
- * @method static \PAO\Routing\Router delete(string $url, array $params)
- * @method static \PAO\Routing\Router head(string $url, array $params)
- * @method static \PAO\Routing\Router options(string $url, array $params)
- * @method static \PAO\Routing\Router share(array $routes)
+ * @method static Router group(array $attributes, Closure $callback)
+ * @method static Router get(string $url, array $params)
+ * @method static Router post(string $url, array $params)
+ * @method static Router put(string $url, array $params)
+ * @method static Router patch(string $url, array $params)
+ * @method static Router delete(string $url, array $params)
+ * @method static Router head(string $url, array $params)
+ * @method static Router options(string $url, array $params)
+ * @method static Router share(array $routes)
  * ------------------------------------------------
  * GET        |  /test               |  index    |
  * GET        |  /test/(:id)         |  select   |
@@ -21,13 +21,13 @@ use Illuminate\Support\Facades\Facade;
  * GET        |  /test/(:id)/modify  |  modify   |
  * PUT/PATCH  |  /test/(:id)         |  update   |
  * DELETE     |  /test/(:id)         |  delete   |
- * @method static \PAO\Routing\Router resource(string $url, string $controller)
- * @method static \PAO\Routing\Router restful(string $url, string $controller)
- * @method static \PAO\Routing\Router regex(string $key, string $regex)
- * @method static \PAO\Routing\Router router(string $tag)
- * @method static \PAO\Routing\Router routes(string $tag)
- * @method static \PAO\Routing\Router groups()
- * @method static \PAO\Routing\Router dispatch()
+ * @method static Router resource(string $url, string $controller)
+ * @method static Router restful(string $url, string $controller)
+ * @method static Router regex(string $key, string $regex)
+ * @method static Router router(string $tag)
+ * @method static Router routes(string $tag)
+ * @method static Router groups()
+ * @method static Router dispatch()
  */
 class Router extends Facade
 {
