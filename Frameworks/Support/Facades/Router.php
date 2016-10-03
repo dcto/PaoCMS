133c20a2939ff7c923024979145581a5fd00bf28
DC <heytsh@gmail.com>
2016-10-03 15:29:38 +0700

    update router facades

--- Frameworks/Support/Facades/Router.php
+++ Frameworks/Support/Facades/Router.php
@@ -2,6 +2,33 @@
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class Router
+ * @method static Router group(array $attributes, Closure $callback)
+ * @method static Router get(string $url, array $params)
+ * @method static Router post(string $url, array $params)
+ * @method static Router put(string $url, array $params)
+ * @method static Router patch(string $url, array $params)
+ * @method static Router delete(string $url, array $params)
+ * @method static Router head(string $url, array $params)
+ * @method static Router options(string $url, array $params)
+ * @method static Router share(array $routes)
+ * ------------------------------------------------
+ * GET        |  /test               |  index    |
+ * GET        |  /test/(:id)         |  select   |
+ * GET        |  /test/create        |  create   |
+ * POST       |  /test               |  insert   |
+ * GET        |  /test/(:id)/modify  |  modify   |
+ * PUT/PATCH  |  /test/(:id)         |  update   |
+ * DELETE     |  /test/(:id)         |  delete   |
+ * @method static Router resource(string $url, string $controller)
+ * @method static Router restful(string $url, string $controller)
+ * @method static Router regex(string $key, string $regex)
+ * @method static Router router(string $tag)
+ * @method static Router routes(string $tag)
+ * @method static Router groups()
+ * @method static Router dispatch()
+ */
 class Router extends Facade
 {
     /**
