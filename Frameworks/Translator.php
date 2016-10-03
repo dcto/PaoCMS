19d4d77cb2b27427e364b5bef5efcbd38a6f80eb
DC <heytsh@gmail.com>
2016-10-03 16:48:56 +0700

    update translator module

--- Frameworks/Translator.php
+++ Frameworks/Translator.php
@@ -134,21 +134,11 @@ class Translator
 
         $SubLanguage = APP.DIRECTORY_SEPARATOR.'Lang'.DIRECTORY_SEPARATOR.NAME.DIRECTORY_SEPARATOR.$lang.'.ini';
 
-        $readable = false;
-
         if(is_readable($AppLanguage)){
             $this->items = (array) parse_ini_file($AppLanguage, true);
-            $readable = true;
         }
-
-        if(is_readable($SubLanguage))
-        {
+        if(is_readable($SubLanguage)) {
             $this->items = array_replace_recursive($this->items, (array) parse_ini_file($SubLanguage, true));
-
-            $readable = true;
         }
-
-        if(!$readable) throw new SystemException('The ['.$lang.'] language was not readable!');
-
     }
 }
