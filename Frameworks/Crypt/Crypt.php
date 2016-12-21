66ce3048baefb717664626950e8ba9605b219757
DC <heytsh@gmail.com>
2016-12-21 22:03:23 +0700

    update crypt module

--- Frameworks/Crypt/Crypt.php
+++ Frameworks/Crypt/Crypt.php
@@ -3,108 +3,91 @@
 namespace PAO\Crypt;
 
 /**
-* DES加密解密类库
-* @author  11
-* @version  1.2
-*/
+ *
+ * 本类按照RC4加密解密算法编写
+ * @author 11
+ * @version 20161222
+ * @link http://en.wikipedia.org/wiki/RC4
+ */
 class Crypt
 {
-	/**
-	 * [$key 密钥]
-	 * @var [type]
-	 */
-	private $key;
-
-	/**
-	 * [$bit 进制]
-	 * @var [type]
-	 */
-	private $bit; //128|256
-
-	/**
-	 * [$iv 偏移量]
-	 * @var [type]
-	 */
-	private $iv;
-
-	/**
-	 * [__construct description]
-	 * @param [type]  $key [密钥]
-	 * @param integer $bit [进制]
-	 * @param [type]  $iv  [偏移量]
-	 */
-	public function __construct($key = null, $bit = 128, $iv = "")
-	{
-	    $key = $key?:config('app.token');
-		$this->key = $bit == 256 ? hash('sha256', $key, true) : hash('crc32b', $key, true);
-        $this->bit = $bit;
-		$this->iv = $iv != "" ? hash('MD5', $iv, true) : str_repeat(chr(0), 16);
-	}
+    /**
+     * 密钥
+     * @var string
+     */
+    private $key;
 
     /**
-     * set get key
-     *
-     * @param $key
+     * Crypt constructor.
      */
-	public function key($key = null)
+    public function __construct()
     {
-        return $key ? $this->key = $key : $this->key;
+        $this->key = config('app.token', 'pao_');
     }
 
     /**
-     * [en 加密字符串]
-     *
-     * @param $str      [明文]
-     * @param bool $key [密钥]
+     * 加密
+     * @param string $key 私匙
+     * @param string $data 需要加密的数据
+     * @param boolean $decrypted 是否解密
+     * @return 16进制字符串
+     */
+    public function en($string, $key = false, $decrypted = false)
+    {
+        $keyLength = strlen($key = $key ?: $this->key);
+        $s = array();
+        for($i = 0; $i < 256; $i++) $s[$i] = $i;
+        $j = 0;
+        for ($i = 0; $i < 256; $i++)
+        {
+            $j = ($j + $s[$i] + ord($key[$i % $keyLength])) % 256;
+            $this->swap($s[$i], $s[$j]);
+        }
+
+        $stringLength = strlen($string);
+        $output = "";
+        for ($a = $j = $i = 0; $i < $stringLength; $i++)
+        {
+            $a = ($a + 1) % 256;
+            $j = ($j + $s[$a]) % 256;
+            $this->swap($s[$a], $s[$j]);
+            $k = $s[(($s[$a] + $s[$j]) % 256)];
+            $output .= chr(ord($string[$i]) ^ $k);
+        }
+
+        return ($decrypted) ? $output : bin2hex($output);
+    }
+    /**
+     * 解密
+     * @param string $a 私匙
+     * @param string $b 需要解密的数据
      * @return string
      */
-	public function en($str, $key = false)
+    public function de($string, $key = false)
     {
-		//Open
-		$module = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_CBC, '');
-		mcrypt_generic_init($module, $this->key(), $this->iv);
-
-		//Padding
-		$block = mcrypt_get_block_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC); //Get Block Size
-		$pad = $block - (strlen($str) % $block); //Compute how many characters need to pad
-		$str .= str_repeat(chr($pad), $pad); // After pad, the str length must be equal to block or its integer multiples
-
-		//Encrypt
-		$encrypted = mcrypt_generic($module, $str);
-
-		//Close
-		mcrypt_generic_deinit($module);
-		mcrypt_module_close($module);
-
-		//Return
-		return bin2hex($encrypted);
-	}
+        $stringLength = strlen($string);
+        if($stringLength % 2){
+            return $string;
+        }else if (strspn($string , '0123456789abcdefABCDEF' ) != $stringLength){
+            return $string;
+        }
+        $key = $key?:$this->key;
+        if (function_exists("hex2bin")){
+            return $this->en(hex2bin($string), $key,  true);
+        }else{//由于hex2bin php5.4才支持采用pack方式处理
+            return $this->en(pack("H*", $string), $key, true);
+        }
+    }
 
     /**
-     * [de 解密字符串]
-     *
-     * @param $str          [密文]
-     * @param bool $key     [密钥]
-     * @return string
+     * 临时缓冲
+     * @param $key
+     * @param $string
      */
-	public function de($str, $key = false)
-	{
-		//Open
-		$module = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_CBC, '');
-		mcrypt_generic_init($module, $this->key(), $this->iv);
-//var_dump(ctype_xdigit($str));die;
-		//Decrypt
-		$str = mdecrypt_generic($module, hex2bin($str)); //Get original str
-
-		//Close
-		mcrypt_generic_deinit($module);
-		mcrypt_module_close($module);
-
-		//Depadding
-		$last = ord(substr($str, -1)); //pad value and pad count
-		$str = substr($str, 0, strlen($str) - $last);
-
-		//Return
-		return $str;
-	}
+    private function swap(&$key, &$string)
+    {
+        $swap = $key;
+        $key = $string;
+        $string = $swap;
+    }
 }
