d08814b3f4de7699c3cbcecfe1c6d0d27b6a91a0
DC <heytsh@gmail.com>
2017-01-20 00:52:02 +0800

    update curl facade

--- Frameworks/Support/Facades/Curl.php
+++ Frameworks/Support/Facades/Curl.php
@@ -4,18 +4,18 @@ use Illuminate\Support\Facades\Facade;
 
 /**
  * Class Curl
- * @method static \PAO\Http\Curl\Curl curl()
- * @method static \PAO\Http\Curl\Curl get(string $url, array $vars = array())
- * @method static \PAO\Http\Curl\Curl post(string $url, array $vars = array(), bool $encrypt = null)
- * @method static \PAO\Http\Curl\Curl options(string $key, array $var = null)
- * @method static \PAO\Http\Curl\Curl headers(string $key, array $var)
- * @method static \PAO\Http\Curl\Curl cookies(string $key, array $var)
- * @method static \PAO\Http\Curl\Curl referer(string $referer)
- * @method static \PAO\Http\Curl\Curl userAgent(string $userAgent)
- * @method static \PAO\Http\Curl\Curl verbose(bool $on = true)
- * @method static \PAO\Http\Curl\Curl retry(int $times = 0)
- * @method static \PAO\Http\Curl\Curl request(string $method, string $url, array $vars = array(), bool $encrypt = null)
- * @method static \PAO\Http\Curl\Curl close()
+ * @method static Curl curl()
+ * @method static Curl get(string $url, array $vars = array())
+ * @method static Curl post(string $url, array $vars = array(), bool $encrypt = null)
+ * @method static Curl options(string $key, array $var = null)
+ * @method static Curl headers(string $key, array $var)
+ * @method static Curl cookies(string $key, array $var)
+ * @method static Curl referer(string $referer)
+ * @method static Curl userAgent(string $userAgent)
+ * @method static Curl verbose(bool $on = true)
+ * @method static Curl retry(int $times = 0)
+ * @method static Curl request(string $method, string $url, array $vars = array(), bool $encrypt = null)
+ * @method static Curl close()
  */
 class Curl extends Facade
 {
